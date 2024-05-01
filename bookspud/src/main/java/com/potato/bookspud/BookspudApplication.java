94218eb37328718d156fdc074970b009fb83f748
1015s <1015_s@naver.com>
2024-05-02 00:36:32 +0900

    Setting: 초기 환경 세팅

---
+++ bookspud/src/main/java/com/potato/bookspud/BookspudApplication.java
@@ -0,0 +1,13 @@
+package com.potato.bookspud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookspudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookspudApplication.class, args);
+	}
+
+}
