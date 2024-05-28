c0ed795483cc16124e40afcedd4ab45b0f0153a3
1015s <1015_s@naver.com>
2024-05-28 11:35:06 +0900

    FEAT: 책 생성 기능 구현

--- bookspud/src/main/java/com/potato/bookspud/domain/book/dto/request/BookCreateRequest.java
+++ bookspud/src/main/java/com/potato/bookspud/domain/book/dto/request/BookCreateRequest.java
@@ -1,2 +1,15 @@
-package com.potato.bookspud.domain.book.dto.request;public class BookCreateRequest {
+package com.potato.bookspud.domain.book.dto.request;
+import lombok.NonNull;
+public record BookCreateRequest(
+        @NonNull
+        String isbn,
+        String title,
+        String author,
+        String genre,
+        String cover,
+        Float price,
+        String content,
+        Integer totalPage,
+        String purchaseLink
+) {
 }
