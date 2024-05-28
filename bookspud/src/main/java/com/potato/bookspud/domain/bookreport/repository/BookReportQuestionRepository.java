9efbdb154c32d5ba91666b03ef54e39f5cbbb731
1015s <1015_s@naver.com>
2024-05-28 11:33:18 +0900

    MODIFY: Repository에 JPA 연결

--- bookspud/src/main/java/com/potato/bookspud/domain/bookreport/repository/BookReportQuestionRepository.java
+++ bookspud/src/main/java/com/potato/bookspud/domain/bookreport/repository/BookReportQuestionRepository.java
@@ -1,4 +1,8 @@
 package com.potato.bookspud.domain.bookreport.repository;
 
-public interface BookReportQuestionRepository {
+import com.potato.bookspud.domain.bookreport.domain.BookReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BookReportQuestionRepository extends JpaRepository<BookReport, Long>{
 }
