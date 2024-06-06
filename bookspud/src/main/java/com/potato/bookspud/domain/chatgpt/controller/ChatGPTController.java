cb5bd894cc0757e5c6872e5ef77c5ce10ac4b950
1015s <1015_s@naver.com>
2024-06-07 04:39:55 +0900

    MODIFY: chatGPT 지시문 수정

--- bookspud/src/main/java/com/potato/bookspud/domain/chatgpt/controller/ChatGPTController.java
+++ bookspud/src/main/java/com/potato/bookspud/domain/chatgpt/controller/ChatGPTController.java
@@ -12,12 +12,12 @@ public class ChatGPTController {
     private final ChatGPTService chatGPTService;
 
     public String makeArguments(List<String> info){
-        String instruction = "이 정보들을 바탕으로 한 독서록을 쓸건데, 제목을 지어줬으면 좋겠어. 간단한 주제를 담은 제목을 5개 생성해줘. 이때 각 문장은 개행문자로 구분해서 줘. 숫자는 붙이지 말고 제목만 줘";
+        String instruction = "이 정보들을 바탕으로 한 독서록을 쓸건데, 제목을 지어줬으면 좋겠어. 간단한 주제를 담은 제목을 5개 생성해줘. 이때 각 문장은 개행문자로 구분해서 줘. 숫자는 붙이지 말고 제목만 줘.";
         return chatGPTService.chat(info, instruction);
     }
 
     public String makeQuestions(List<String> info){
-        String instruction = "이 정보들을 바탕으로 서론, 본론, 결론으로 나누어서 질문을 각각 1개씩 만들어줘. 이때 각 질문은 개행문자로 구분해서 줘";
+        String instruction = "이 정보들을 바탕으로 서론, 본론, 결론으로 나누어서 질문을 각각 1개씩 만들어줘. 서론: 이런거 붙이지말고, 서론-본론-결론 순서대로 질문만 간단히 생성해줘. 이때 각 질문은 개행문자로 구분해서 줘";
         return chatGPTService.chat(info, instruction);
     }
 
