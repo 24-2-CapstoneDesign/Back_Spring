55f71afcf03b59f7223499cb6342ffcd192c141c
1015s <1015_s@naver.com>
2024-06-07 04:39:23 +0900

    MODIFY: birthyear로 user 조회 기능 수정

--- bookspud/src/main/java/com/potato/bookspud/domain/user/repository/UserRepository.java
+++ bookspud/src/main/java/com/potato/bookspud/domain/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.potato.bookspud.domain.user.repository;
 
 import com.potato.bookspud.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findById(long id);
     Optional<User> findBySocialId(Long socialId);
+    @Query("SELECT u FROM User u WHERE u.birthyear = :birthyear AND u.id <> :userId ORDER BY u.updatedAt DESC")
+    User findTopByBirthyearAndIdNotOrderByUpdatedAtDesc(@Param("birthyear") String birthyear, @Param("userId") Long userId);
 }
